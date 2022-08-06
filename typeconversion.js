1ed867d44e30d2319d8d35feac40ad5e95663097
Medni1061 <105156031+Medni1061@users.noreply.github.com>
2022-08-06 17:57:50 +0530

    Add files via upload

---
+++ typeconversion.js
@@ -0,0 +1,10 @@
+document.write(+number("3.1493002893"));
+document.write(+number("  "));
+document.write(+number(""));
+document.write(+number("98" "88"));
+
+let num=100;
+let n=25;
+let s=string("100+23");
+document.write("</br>"+string(num));
+document.write("</br>"+string());
